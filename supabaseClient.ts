011d4917844c17ded588acf2819026b3e69ed803
pablodriussi-coder <pablodriussi@gmail.com>
2026-01-26 20:42:39 -0300

    Add files via upload

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -10,4 +10,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://zddsaglknuryxronezdzp.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkZHNhZ2xrbnVyeHJvbmV6ZHpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0Mzc5NTMsImV4cCI6MjA4NTAxMzk1M30._ob0giV5-J6TCQUYlXoNvqRBsFR7Fradciq6Ujev_Lw';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
